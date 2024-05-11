0445ebad23081fc5c0def6798fd6584e40443efc
MoAdel <madel7586@gmail.com>
2024-05-11 16:15:25 +0300

    chore: Update custom book card widget to use AspectRatio and remove unnecessary SizedBox

--- lib/features/home/presentation/views/widgets/custom_book_card.dart
+++ lib/features/home/presentation/views/widgets/custom_book_card.dart
@@ -5,18 +5,15 @@ class CustomBookCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * 0.3,
-      child: AspectRatio(
-        aspectRatio: 2.7 / 4,
-        child: Container(
-          decoration: const BoxDecoration(
-            borderRadius: BorderRadius.all(Radius.circular(16)),
-            color: Colors.red,
-            image: DecorationImage(
-              image: AssetImage('assets/images/test_image.png'),
-              fit: BoxFit.cover,
-            ),
+    return AspectRatio(
+      aspectRatio: 2.7 / 4,
+      child: Container(
+        decoration: const BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(16)),
+          color: Colors.red,
+          image: DecorationImage(
+            image: AssetImage('assets/images/test_image.png'),
+            fit: BoxFit.cover,
           ),
         ),
       ),
