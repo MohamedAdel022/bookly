144bf5c7d3e761fdabfc73128ea4951fda1b95ca
MoAdel <madel7586@gmail.com>
2024-05-11 14:05:19 +0300

    feat: Add sliding text animation to splash view body and navigate to home view after delay

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,7 +1,9 @@
+import 'package:bookly/features/splash/presentation/views/home/presentation/views/view_model/home_view.dart';
 import 'package:bookly/features/splash/presentation/views/widgets/sliding_text.dart';
 import 'package:flutter/material.dart';
 import 'package:bookly/core/utils/assets.dart';
 import 'package:flutter/widgets.dart';
+import 'package:get/get.dart';
 
 class SplasViewhBody extends StatefulWidget {
   const SplasViewhBody({super.key});
@@ -17,6 +19,32 @@ class _SplasViewhBodyState extends State<SplasViewhBody>
   @override
   void initState() {
     super.initState();
+    initSlidingAnimation();
+
+    navigateHome();
+  }
+
+
+  @override
+  void dispose() {
+    super.dispose();
+    animationController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Image.asset(AssetData.logo),
+        const SizedBox(height: 20),
+        SlidingTextAnimation(slidingAnimation: slidingAnimation),
+      ],
+    );
+  }
+
+  void initSlidingAnimation() {
     animationController = AnimationController(
       vsync: this,
       duration: const Duration(seconds: 2),
@@ -34,25 +62,11 @@ class _SplasViewhBodyState extends State<SplasViewhBody>
 
     animationController.forward();
   }
-
-  @override
-  void dispose() {
-    // TODO: implement dispose
-    super.dispose();
-    animationController.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        Image.asset(AssetData.logo),
-        const SizedBox(height: 20),
-        SlidingTextAnimation(slidingAnimation: slidingAnimation),
-      ],
-    );
-  }
 }
-
+  void navigateHome() {
+    Future.delayed(const Duration(seconds: 3), () {
+      Get.to(() => const HomeView(),
+          transition: Transition.fade,
+          duration: const Duration(milliseconds: 500));
+    });
+  }
